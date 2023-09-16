e6729eb6b23061d82e66cdc066d6c09a639e6c08
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-09-16 16:36:01 +0600

    add global styles

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ToastContainer } from "react-toastify";
 import { withRedux, withRouter } from "./components/ui/HOC";
 import AppRouter from "./router/AppRouter";
+import "./styles/app.css";
 
 const App = () => {
     return (
